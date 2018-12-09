59d4e497bfd047a521a560432785dc0cd1a1893f
Ezio <assassinste@gmail.com>
2018-12-09 09:43:07 +0800

    Sending-mail: gen a valid share-url & the page itself

--- Chapter_01/blog/urls.py
+++ Chapter_01/blog/urls.py
@@ -6,9 +6,12 @@ app_name = 'blog'
 
 urlpatterns = [
     # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('',
+         views.PostListView.as_view(), name='post_list'),
     
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-         views.post_detail,
-         name='post_detail'),
+         views.post_detail, name='post_detail'),
+    
+    path('<int:post_id>/share/',
+         views.post_share, name='post_share'),
 ]
