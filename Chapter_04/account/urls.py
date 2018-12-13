54ace46482236dcb7d1f087f71d4ead2f86bd097
Ezio <assassinste@gmail.com>
2018-12-13 23:58:48 +0800

    Reset-pwd: now it's fully functional

--- Chapter_04/account/urls.py
+++ Chapter_04/account/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
          dj_auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/',
          dj_auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/',
+         dj_auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/',
+         dj_auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
