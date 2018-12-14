2286513554816a9e0fbf87b9dd25bb267de39936
Ezio <assassinste@gmail.com>
2018-12-14 09:47:22 +0800

    User-register: nearly finished (storeToDB, welcomeMsg)

--- Chapter_04/04_TO_BE_RENAMED.md
+++ Chapter_04/04_TO_BE_RENAMED.md
@@ -3,6 +3,9 @@
 - **Registration** is coming!
 
 ### Let's setup for it 
-1. First we'll need user's info (therefore by using a *form*)
+1. First we'll need user's info (therefore by using a *form* (i.e. ```forms.py```))
 2. And the *logic* in the ```views.py``` (app-level)
-3. 
+3. the basic route on the ```urls.py``` (app-level)
+4. and two templates (under ```templates/account/```)
+    - ```register.html``` (display form)
+    - ```register_done.html``` (welcome msg (optional))
--- Chapter_04/account/views.py
+++ Chapter_04/account/views.py
@@ -36,7 +36,20 @@ def user_login(request):
 
 
 def register(request):
-    user_form = UserRegistrationForm()
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            
+            new_user.save()
+            
+            return render(request,
+                          'account/register_done.html', { 'new_user': new_user })
+    
+    else:
+        user_form = UserRegistrationForm()
     
     return render(request,
                   'account/register.html', { 'user_form': user_form })
