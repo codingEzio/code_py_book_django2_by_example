3b5fcf6687ee8d7229ec6ad1f2af2ed5ee987686
Ezio <assassinste@gmail.com>
2018-12-16 22:23:40 +0800

    Add missing features & typo fixed

--- Chapter_04/images/static/js/bookmarklet.js
+++ Chapter_04/images/static/js/bookmarklet.js
@@ -8,16 +8,49 @@
     var min_height = 100;
 
     function bookmarklet(msg) {
-        // pass
+
+        // load CSS
+        var css = jQuery('<link>');
+        css.attr({
+            rel: 'stylesheet',
+            type: 'text/css',
+            href: static_url
+                + 'css/bookmarklet.css?r='
+                + Math.floor(Math.random() * 99999999999999)
+        });
+
+        jQuery('head').append(css);
+
+        // load HTML
+        box_html = '<div id="bookmarklet"><a href="#" id="close">&times;</a> +<h1>Select an image to bookmark:</h1><div class="images"></div></div>';
+        jQuery('body').append(box_html);
+
+        // close event
+        jQuery('#bookmarklet #close').click(function () {
+            jQuery('#bookmarklet').remove();
+        });
+
+        // find images and display them
+        jQuery.each(jQuery('img[src$="jpg"]'), function (index, image) {
+            if (
+                jQuery(image).width() >= min_width &&
+                jQuery(image).height() >= min_height) {
+                image_url = jQuery(image).attr('src');
+                jQuery('#bookmarklet .images').append(
+                    '<a href="#"><img src="' + image_url + '" /></a>'
+                );
+            }
+        });
+
     }
 
-    if (typeof window.jquery != 'undefined') {
+    if (typeof window.jQuery != 'undefined') {
         bookmarklet();
     } else {
         var conflict = typeof window.$ != 'undefined';
         var script = document.createElement('script');
 
-        script.src = '//ajax.googleapis.com/ajax/libs/jquery'
+        script.src = '//ajax.googleapis.com/ajax/libs/jquery/'
             + jquery_version
             + '/jquery.min.js';
         document.head.appendChild(script);
@@ -25,7 +58,7 @@
         var attempts = 15;
 
         (function () {
-            if (typeof window.jquery == 'undefined') {
+            if (typeof window.jQuery == 'undefined') {
                 if (--attempts > 0) {
                     window.setTimeout(arguments.callee, 250);
                 } else {
