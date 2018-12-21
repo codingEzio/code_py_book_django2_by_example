07cb91b10a79808fe8fc7457dbb309cfa32e5a8a
Ezio <assassinste@gmail.com>
2018-12-21 09:15:40 +0800

    Adding logic & jQuery-in-tmpl-as-AJAX (follower-sys)

--- Chapter_04/account/views.py
+++ Chapter_04/account/views.py
@@ -1,17 +1,19 @@
 # coding=utf-8
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 
+from common.decorators import ajax_required
 from .forms import (
     LoginForm, UserRegistrationForm,
     UserEditForm, ProfileEditForm,
 )
 
-from .models import Profile, User
+from .models import Profile, User, Contact
 
 
 def user_login(request):
@@ -115,3 +117,36 @@ def user_detail(request, username):
     return render(request,
                   'account/user/detail.html', { 'section': 'people',
                                                 'user'   : user })
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    """
+        This one is VERY similar to the <image_like> func in app-image/views.
+        
+        There are only two options after all (well, true for some cases) :D
+    """
+    
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+            
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+                
+            return JsonResponse({'status': 'ok'})
+        
+        except User.DoesNotExist:
+            return JsonResponse({ 'status': 'ko' })
+    
+    return JsonResponse({ 'status': 'ko' })
