ca6cd5c17580660d763eec3d14ed923dca67c33b
Ezio <assassinste@gmail.com>
2018-12-21 09:14:32 +0800

    Adding route for 'user-follow-each-other

--- Chapter_04/account/urls.py
+++ Chapter_04/account/urls.py
@@ -45,6 +45,8 @@ urlpatterns = [
     
     # routes for 'follower system'
     path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+    
     path('user/<username>/', views.user_detail, name='user_detail'),
     
 ]
