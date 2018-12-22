3de87c4efa5e9357dbd2f5052b92ec7d6e12e53a
Ezio <assassinste@gmail.com>
2018-12-22 17:12:43 +0800

    New stuff 'contenttype' (prevent 'dup-actions' in our cases)

--- Chapter_04/actions/models.py
+++ Chapter_04/actions/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -13,6 +15,19 @@ class Action(models.Model):
                              on_delete=models.CASCADE)
     
     verb = models.CharField(max_length=255)
+    
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE)
+    
+    target_id = models.PositiveIntegerField(null=True,
+                                            blank=True,
+                                            db_index=True)
+    
+    target = GenericForeignKey('target_ct', 'target_id')
+    
     created = models.DateTimeField(auto_now_add=True,
                                    db_index=True)
     
