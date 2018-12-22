db1f2bb43f699c860bdbf3ede329011e10fa9e14
Ezio <assassinste@gmail.com>
2018-12-22 15:49:23 +0800

    Model of app 'actions' inited (X does Y at TIME)

--- Chapter_04/actions/models.py
+++ Chapter_04/actions/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 
+
+class Action(models.Model):
+    """
+        In short, this model stores "X did Y in TIME" :D
+        
+    """
+    
+    user = models.ForeignKey('auth.User',
+                             related_name='actions',
+                             db_index=True,
+                             on_delete=models.CASCADE)
+    
+    verb = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True,
+                                   db_index=True)
+    
+    class Meta:
+        ordering = ('-created',)
