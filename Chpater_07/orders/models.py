838a943edf93ba59aec19fc5586922c67aaeef15
Ezio <assassinste@gmail.com>
2018-12-28 07:22:11 +0800

    New models added (order, orderItem)

--- Chpater_07/orders/models.py
+++ Chpater_07/orders/models.py
@@ -1,2 +1,57 @@
 from django.db import models
 
+from shop.models import Product
+
+
+class Order(models.Model):
+    """
+        Store the order details :D
+    """
+    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    paid = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ('-created',)
+    
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+    
+    def get_total_cost(self):
+        return sum(
+            item.get_cost()
+            for item in self.items.all()
+        )
+
+
+class OrderItem(models.Model):
+    """
+        Store items bought (price, quantity)
+    """
+    
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return '{}'.format(self.id)
+    
+    def get_cost(self):
+        return self.price * self.quantity
