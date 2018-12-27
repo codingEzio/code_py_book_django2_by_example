3cd9fbe93fe5d97e128fb1bc882cc0e58cdde864
Ezio <assassinste@gmail.com>
2018-12-27 10:36:48 +0800

    Basic functionalities for 'cart' (add/update, detail)

--- Chpater_07/cart/views.py
+++ Chpater_07/cart/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from shop.models import Product
+
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+@require_POST
+def cart_add(request, product_id):
+    """
+        ---- Lemme break down this function ----
+
+        <cart, product>
+            Init a new cart inst 
+            Query the product u added (by ID)
+
+        <form, cleaned>
+            Init a 'cardAdd' form ('POST')
+
+        <card.add>
+            Submit with 'product, quantity' and 'update-or-add'    
+    """
+    
+    cart = Cart(request)
+    
+    product = get_object_or_404(Product, id=product_id)
+    
+    form = CartAddProductForm(request.POST)
+    
+    if form.is_valid():
+        input_data = form.cleaned_data
+        
+        cart.add(product=product,
+                 quantity=input_data['quantity'],
+                 update_quantity=input_data['update'])
+    
+    return redirect('cart:cart_detail')
+
+
+def cart_remove(request, product_id):
+    """
+    
+    """
+    
+    cart = Cart(request)
+    
+    product = get_object_or_404(Product, id=product_id)
+    
+    cart.remove(product)
+    
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    """
+    
+    """
+    
+    cart = Cart(request)
+    
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={
+                'quantity': item['quantity'],
+                'update'  : True
+            }
+        )
+    
+    return render(request,
+                  'cart/detail.html', { 'cart': cart })
