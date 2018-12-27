63e8a7d49e4d24d5f34482f35746af075fded268
Ezio <assassinste@gmail.com>
2018-12-27 10:37:38 +0800

    An entry was added ('add-to-cart')

--- Chpater_07/shop/views.py
+++ Chpater_07/shop/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
 
+from cart.forms import CartAddProductForm
+
 
 def product_list(request, category_slug=None):
     category = None
@@ -25,5 +27,8 @@ def product_detail(request, id, slug):
                                 slug=slug,
                                 available=True)
     
+    cart_product_form = CartAddProductForm()
+    
     return render(request,
-                  'shop/product/detail.html', { 'product': product, })
+                  'shop/product/detail.html', { 'product'          : product,
+                                                'cart_product_form': cart_product_form })
