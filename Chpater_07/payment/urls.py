88495d6783970b30931cc9aabaaba2e751079617
Ezio <assassinste@gmail.com>
2018-12-30 23:15:00 +0800

    Pre-setup for the basic scenarios (fail/success)

--- Chpater_07/payment/urls.py
+++ Chpater_07/payment/urls.py
@@ -1,2 +1,13 @@
+from django.urls import path
+
+from . import views
 
 app_name = 'payment'
+
+urlpatterns = [
+    path('done/',
+         views.payment_done, name='done'),
+    
+    path('canceled/',
+         views.payment_canceled, name='canceled'),
+]
--- Chpater_07/payment/views.py
+++ Chpater_07/payment/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+import braintree
+
+from orders.models import Order
+
+
+def payment_done(request):
+    """
+    
+    """
+    
+    return render(request,
+                  'payment/done.html')
+
+
+def payment_canceled(request):
+    """
+    
+    """
+    
+    return render(request,
+                  'payment/canceled.html')
