a4515f7c9aaf2cb59fb57909aecbb275941d3b73
Ezio <assassinste@gmail.com>
2019-01-01 01:37:04 +0800

    An entry added (not webpage but the admin)

--- Chpater_07/orders/admin.py
+++ Chpater_07/orders/admin.py
@@ -4,6 +4,9 @@ import datetime
 from django.contrib import admin
 from django.http import HttpResponse
 
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
 from .models import Order, OrderItem
 
 
@@ -59,12 +62,27 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_detail(obj):
+    """
+
+    """
+    
+    return mark_safe(
+        '<a href="{}">View</a>'.format(
+            reverse(
+                'orders:admin_order_detail', args=[obj.id]
+            )
+        )
+    )
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city',
                     'paid',
-                    'created', 'updated']
+                    'created', 'updated',
+                    order_detail]
     
     list_filter = ['paid', 'created', 'updated']
     
