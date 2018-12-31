282eb454ad72b6da1a72621bc3418a8c93129bb8
Ezio <assassinste@gmail.com>
2018-12-31 20:27:33 +0800

    Adding 'export-to-csv' at admin site (by customizing 'admin.py')

--- Chpater_07/orders/admin.py
+++ Chpater_07/orders/admin.py
@@ -1,4 +1,8 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Order, OrderItem
 
@@ -6,6 +10,53 @@ from .models import Order, OrderItem
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+        The three params are REQUIRED :D
+    """
+    
+    opts = modeladmin.model._meta
+    
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = (
+        'attachment;'
+        'filename={}.csv'.format(opts.verbose_name)
+    )
+    
+    # Init a CSV file
+    #   of course it got a name (ie `opts.verbose_name`)
+    writer = csv.writer(response)
+    
+    # The M-to-M & N-to-M relationship will be excluded :D
+    fields = [
+        field for field in opts.get_fields()
+        if not field.many_to_many and
+           not field.one_to_many
+    ]
+    
+    # The header for the CSV file
+    writer.writerow(
+        [field.verbose_name for field in fields]
+    )
+    
+    for obj in queryset:
+        data_row = []
+        
+        for field in fields:
+            value = getattr(obj, field.name)
+            
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            
+            data_row.append(value)
+        writer.writerow(data_row)
+    
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 @admin.register(Order)
@@ -18,3 +69,6 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ['paid', 'created', 'updated']
     
     inlines = [OrderItemInline]
+    
+    # csv related
+    actions = [export_to_csv]
