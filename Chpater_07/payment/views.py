7008564a68fcc1df6e6c7353dab12b7eae9b9502
Ezio <assassinste@gmail.com>
2018-12-31 17:01:49 +0800

    The core part was done (you could REALLY 'checkout' now)

--- Chpater_07/payment/views.py
+++ Chpater_07/payment/views.py
@@ -5,6 +5,44 @@ import braintree
 from orders.models import Order
 
 
+def payment_process(request):
+    """
+    
+    """
+    
+    # Simply to find/get what we need
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    
+    if request.method == 'POST':
+        nonce = request.POST.get('payment_method_nonce', None)
+        
+        result = braintree.Transaction.sale({
+            'amount'              : '{:.2f}'.format(order.get_total_cost()),
+            'payment_method_nonce': nonce,
+            'options'             : { 'submit_for_settlement': True, }
+        })
+        
+        if result.is_success:
+            order.paid = True
+            order.braintree_id = result.transaction.id
+            
+            order.save()
+            
+            return redirect('payment:done')
+        
+        else:
+            return redirect('payment:canceled')
+    
+    else:
+        
+        client_token = braintree.ClientToken.generate()
+        
+        return render(request,
+                      'payment/process.html', { 'order'       : order,
+                                                'client_token': client_token })
+
+
 def payment_done(request):
     """
     
