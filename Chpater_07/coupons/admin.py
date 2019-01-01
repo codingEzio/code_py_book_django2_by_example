e26be4ecf49b725f57d92248e475dbbbc55e4d6c
Ezio <assassinste@gmail.com>
2019-01-01 21:46:10 +0800

    Registing the 'coupon' model at admin site

--- Chpater_07/coupons/admin.py
+++ Chpater_07/coupons/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Coupon
+
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'active',
+                    'discount',
+                    'valid_from', 'valid_to', ]
+    list_filter = ['active',
+                   'valid_from', 'valid_to']
+    
+    search_fields = ['code']
+
+
+admin.site.register(Coupon, CouponAdmin)
