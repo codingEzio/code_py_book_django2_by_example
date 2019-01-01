c16a546d96e7260885756a3dc587a3ad327d13cf
Ezio <assassinste@gmail.com>
2019-01-01 12:32:51 +0800

    Setup routes for 'gen-pdf-dynamically'

--- Chpater_07/orders/urls.py
+++ Chpater_07/orders/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path('admin/order/<int:order_id>',
          views.admin_order_detail, name='admin_order_detail'),
     
+    path('admin/order/<int:order_id>/pdf/',
+         views.admin_order_pdf, name='admin_order_pdf'),
     
+    # path('',
+    #      views., name='')
 ]
