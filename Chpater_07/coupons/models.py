bc6483ec41e262c6f7a46f3ee9134a065153b27a
Ezio <assassinste@gmail.com>
2019-01-01 21:45:58 +0800

    Adding new models for our 'coupons|discount'

--- Chpater_07/coupons/models.py
+++ Chpater_07/coupons/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import (MinValueValidator,
+                                    MaxValueValidator)
 
-# Create your models here.
+
+class Coupon(models.Model):
+    """
+    
+    """
+    
+    # the code for discount XD
+    code = models.CharField(max_length=50,
+                            unique=True)
+    
+    # the time that indicates whether it's valid or not
+    #   e.g. From <Dec.01> To <Dec.31>
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    
+    # percentage to the price
+    discount = models.IntegerField(validators=[MinValueValidator,
+                                               MaxValueValidator])
+    
+    # simply you could use it or not
+    active = models.BooleanField()
+    
+    def __str__(self):
+        return self.code
