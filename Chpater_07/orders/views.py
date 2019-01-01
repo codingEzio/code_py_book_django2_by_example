59605f6d44145490fb01b4de0076c307ff93866f
Ezio <assassinste@gmail.com>
2019-01-01 12:47:59 +0800

    The core stuff for generating the PDF!

--- Chpater_07/orders/views.py
+++ Chpater_07/orders/views.py
@@ -2,14 +2,21 @@ from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
 from .models import Order, OrderItem
 from .forms import OrderCreateForm
 
 # Don't use `.tasks` in here
-#   Celery does NOT recognize the rel path, the one we're using is "package".
+#   Celery does NOT recognize the relative path,
+#   the one we're using is letting Celery treating it as "package".
 from orders.tasks import order_created
 
 from cart.cart import Cart
+
+import weasyprint
 
 
 def order_create(request):
@@ -61,3 +68,32 @@ def admin_order_detail(request, order_id):
     
     return render(request,
                   'admin/orders/order/detail.html', { 'order': order })
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    """
+        1. Get data (and templates for displaying data)
+        2. Set type (cuz you'll need to download it, right?)
+        3. Using the module (configuring stuff, e.g. the CSS :P)
+    """
+    
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            { 'order': order })
+    
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = (
+        'filename=order_{}.pdf'.format(order.id)
+    )
+    
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[
+            weasyprint.CSS(
+                settings.STATIC_ROOT + 'css/pdf.css'
+            )
+        ]
+    )
+    
+    return response
