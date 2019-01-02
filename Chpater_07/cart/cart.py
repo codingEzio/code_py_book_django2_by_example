2a37892d4b6a082a4f43cbd877a2eaaf6ee44c93
Ezio <assassinste@gmail.com>
2019-01-02 08:50:13 +0800

    One prop & two methods added (calc 'discount')

--- Chpater_07/cart/cart.py
+++ Chpater_07/cart/cart.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from django.conf import settings
 
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart(object):
@@ -29,6 +30,8 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = { }
         
         self.cart = cart
+        
+        self.coupon_id = self.session.get('coupon_id')
     
     def add(self, product, quantity=1, update_quantity=False):
         """
@@ -141,3 +144,19 @@ class Cart(object):
         del self.session[settings.CART_SESSION_ID]
         
         self.save()
+    
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        
+        return None  # exec if not getting ID
+    
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100')) * self.get_total_price()
+        
+        return Decimal('0')  # no discount if there's no coupon
+    
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
