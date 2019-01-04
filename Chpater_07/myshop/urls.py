9fbfca450190107039f714b9f74d66c5a758e07a
Ezio <assassinste@gmail.com>
2019-01-04 10:33:13 +0800

    Add 'lang prefix' to urlpatterns (e.g. /en /zh-hant)

--- Chpater_07/myshop/urls.py
+++ Chpater_07/myshop/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+
 from django.contrib import admin
+
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     
     # These routes MUST be put before the 'shop.urls'
@@ -16,7 +19,7 @@ urlpatterns = [
     path('rosetta/', include('rosetta.urls')),
     
     path('', include('shop.urls', namespace='shop')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
