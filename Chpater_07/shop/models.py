d2bda89bd693123cf2d7dc7a924e4dd0e7921cd4
Ezio <assassinste@gmail.com>
2019-01-05 14:35:03 +0800

    [NOTHING-NEW]

--- Chpater_07/shop/models.py
+++ Chpater_07/shop/models.py
@@ -26,12 +26,12 @@ class Category(TranslatableModel):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
     
+    def __str__(self):
+        return self.name
+    
     def get_absolute_url(self):
         return reverse('shop:product_list_by_category',
                        args=[self.slug])
-    
-    def __str__(self):
-        return self.name
 
 
 class Product(TranslatableModel):
@@ -39,15 +39,15 @@ class Product(TranslatableModel):
     
     """
     
-    category = models.ForeignKey(Category,
-                                 related_name='products',
-                                 on_delete=models.CASCADE)
-    
     translations = TranslatedFields(
         name=models.CharField(max_length=200, db_index=True),
         slug=models.SlugField(max_length=200, db_index=True),
         description=models.TextField(blank=True),
     )
+    
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
     
     image = models.ImageField(upload_to='products/%Y/%m/%d',
                               blank=True)
@@ -70,10 +70,11 @@ class Product(TranslatableModel):
         
         # ordering = ('name',)
         # index_together = (('id', 'slug'),)
+        pass
+    
+    def __str__(self):
+        return self.name
     
     def get_absolute_url(self):
         return reverse('shop:product_detail',
                        args=[self.id, self.slug])
-    
-    def __str__(self):
-        return self.name
