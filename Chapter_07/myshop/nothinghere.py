116c841294877a9f0d868d7bdb8f284a10db5787
Ezio <assassinste@gmail.com>
2019-01-14 17:03:59 +0800

    [NOTHING-NEW] (typo fixed)

---
+++ Chapter_07/myshop/nothinghere.py
@@ -0,0 +1,7 @@
+BRT_MERCHANT_ID = 'xt9n8pfwyrmrpf9d'
+BRT_PUBLIC_KEY  = 'vtx85nk68m7ykhc7'
+BRT_PRIVATE_KEY = 'b81e403ea908c6221714d5f82e78264a'
+
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_HOST_USER = 'appleincceo@qq.com'
+EMAIL_HOST_PASSWORD = 'kmhhwvjwpoimbgja'
