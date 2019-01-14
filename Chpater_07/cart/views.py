67770433a6fa4c3c46dbdb15a1ed8f771c70acf3
Ezio <assassinste@gmail.com>
2019-01-14 16:39:21 +0800

    Display the 'recommendation' at cart page

--- Chpater_07/cart/views.py
+++ Chpater_07/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 
 from shop.models import Product
+from shop.recommender import Recommender
 
 from .cart import Cart
 from .forms import CartAddProductForm
@@ -64,14 +65,21 @@ def cart_detail(request):
     
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(
-            initial={
-                'quantity': item['quantity'],
-                'update'  : True
-            }
+                initial={
+                    'quantity': item['quantity'],
+                    'update'  : True
+                }
         )
     
     coupon_apply_form = CouponApplyForm()
     
+    recmdr = Recommender()
+    
+    cart_products = [item['product'] for item in cart]
+    recommended_products = recmdr.suggest_products_for(cart_products,
+                                                       max_results=4)
+    
     return render(request,
-                  'cart/detail.html', { 'cart'             : cart,
-                                        'coupon_apply_form': coupon_apply_form })
+                  'cart/detail.html', { 'cart'                : cart,
+                                        'coupon_apply_form'   : coupon_apply_form,
+                                        'recommended_products': recommended_products })
