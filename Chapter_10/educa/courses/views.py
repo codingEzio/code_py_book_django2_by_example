54d5a11ff3c16e2f8da5f2e084c129fd3d591613
Ezio <assassinste@gmail.com>
2019-01-16 17:06:56 +0800

    Impl_ing the functionalities for 'add-module-to-course'

--- Chapter_10/educa/courses/views.py
+++ Chapter_10/educa/courses/views.py
@@ -1,5 +1,8 @@
 from django.urls import reverse_lazy
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+
+from django.views.generic.base import (View,
+                                       TemplateResponseMixin)
 
 from django.views.generic.list import ListView
 from django.views.generic.edit import (CreateView,
@@ -10,10 +13,7 @@ from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
 
 from .models import Course
-
-
-# This is the only change we've made
-#   that is "Making users can only CRUD their own courses"
+from .forms import ModuleFormSet
 
 
 class OwnerMixin(object):
@@ -110,3 +110,45 @@ class CourseDeleteView(PermissionRequiredMixin,
     success_url         = reverse_lazy('manage_course_list')
     
     permission_required = 'courses.delete_course'
+    
+    
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    """
+        As the class name suggests, this view(class)
+            handles the `formset` to {add, update, delete} modules for a course.
+        
+        HOWTO :: distinguish two inherited classes
+            Ours                    get_formset, course, formset
+            View                    dispatch, get, post
+            TemplateResponseMixin   `render_to_response` (inside `get` & `post`)
+    """
+    
+    template_name   = 'courses/manage/module/formset.html'
+    course          = None
+    
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course,
+                             data=data)
+    
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        owner=request.user)
+        
+        return super(CourseModuleUpdateView, self).dispatch(request, pk)
+    
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        
+        return self.render_to_response({'course'    : self.course,
+                                        'formset'   : formset})
+    
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        
+        return self.render_to_response({'course'    : self.course,
+                                        'formset'   : formset})
