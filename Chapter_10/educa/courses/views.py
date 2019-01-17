499f102686d814fe246e6475347b8e680de162ef
Ezio <assassinste@gmail.com>
2019-01-17 15:05:20 +0800

    Delete contents from specifc modules :D

--- Chapter_10/educa/courses/views.py
+++ Chapter_10/educa/courses/views.py
@@ -288,3 +288,18 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
         # Done here if 'update'
         return self.render_to_response({'form'      : form,
                                         'object'    : self.obj})
+        
+        
+class ContentDeleteView(View):
+    
+    def post(self, request, id):
+        content = get_object_or_404(Content, id=id,
+                                    module__course__owner=request.user)
+        
+        # fk related
+        module = content.module
+        
+        content.item.delete()
+        content.delete()
+        
+        return redirect('module_content_list', module.id)
