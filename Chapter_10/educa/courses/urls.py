a96d9041520290f93a4951049019c02a4a63a908
Ezio <assassinste@gmail.com>
2019-01-17 18:12:05 +0800

    Setup routes for the 'content-list' stuff

--- Chapter_10/educa/courses/urls.py
+++ Chapter_10/educa/courses/urls.py
@@ -18,7 +18,9 @@ urlpatterns = [
          views.ContentCreateUpdateView.as_view(), name='module_content_create'),
     path('module/<int:module_id>/content/<model_name>/<id>/',
          views.ContentCreateUpdateView.as_view(), name='module_content_update'),
-    
     path('content/<int:id>/delete/',
-         views.ContentDeleteView.as_view(), name='module_content_delete')
+         views.ContentDeleteView.as_view(), name='module_content_delete'),
+    
+    path('module/<int:module_id>/',
+         views.ModuleContentListView.as_view(), name='module_content_list'),
 ]
