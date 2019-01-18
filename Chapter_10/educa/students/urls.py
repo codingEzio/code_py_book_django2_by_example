f9c74c3346863541fef46ad9021c40f9f4fc73ad
Ezio <assassinste@gmail.com>
2019-01-18 20:45:17 +0800

    URL routes for 'enrolling purposes'

--- Chapter_10/educa/students/urls.py
+++ Chapter_10/educa/students/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('register/',
          views.StudentRegistrationView.as_view(), name='student_registration'),
     
+    path('enroll-course/',
+         views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
 ]
