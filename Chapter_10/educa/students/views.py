2cf55704480c44098eef7e7c7a854366a5f2f57a
Ezio <assassinste@gmail.com>
2019-01-18 20:44:38 +0800

    Let user actually 'enroll' ( & being redirected )

--- Chapter_10/educa/students/views.py
+++ Chapter_10/educa/students/views.py
@@ -1,9 +1,14 @@
 from django.urls import reverse_lazy
 
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import (CreateView,
+                                       FormView)
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import CourseEnrollForm
 
 
 class StudentRegistrationView(CreateView):
@@ -27,3 +32,39 @@ class StudentRegistrationView(CreateView):
         login(self.request, user)
         
         return result
+    
+    
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    """
+    
+    """
+    
+    course          = None
+    form_class      = CourseEnrollForm
+    
+    def form_valid(self, form):
+        """
+            Get the course -> add the student to the course
+            
+            The logic is not that hard,
+                but .. the abstraction is kind weird to me ( the way getting data )
+        """
+        
+        # `ModelChoiceField` while was hidden
+        self.course = form.cleaned_data['course']
+        
+        self.course.students.add(self.request.user)
+        
+        return super(StudentEnrollCourseView,
+                     self).form_valid(form)
+    
+    def get_success_url(self):
+        """
+            attr::success_url == method::get_success_url
+        """
+        
+        # The page that user being redirected after he(she) enrolled
+        #   Well, the page hasn't been impl_ed yet #TODO impl needed, XD
+        return reverse_lazy('student_course_detail',
+                            args=[self.course.id])
+    
