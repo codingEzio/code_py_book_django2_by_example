0d23bbe98b32d88efd437241cfdc6b82fec54191
Ezio <assassinste@gmail.com>
2019-01-21 18:00:07 +0800

    Caching data :: views :: app-students

--- Chapter_10/educa/students/urls.py
+++ Chapter_10/educa/students/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from . import views
 
@@ -11,8 +12,13 @@ urlpatterns = [
     
     path('courses/',
             views.StudentCourseListView.as_view(),      name='student_course_list'),
+    
     path('course/<pk>/',
-            views.StudentCourseDetailView.as_view(),    name='student_course_detail'),
+         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+         name='student_course_detail'),
+    
     path('course/<pk>/<module_id>/',
-            views.StudentCourseDetailView.as_view(),    name='student_course_detail_module'),
+         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+         name='student_course_detail_module'),
+    
 ]
