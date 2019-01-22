4dbbdefcd627020cdc795483d27479dfac5bd772
Ezio <assassinste@gmail.com>
2019-01-22 15:55:57 +0800

    Now it could display the nested fields (Course::Module)

--- Chapter_10/educa/courses/api/serializers.py
+++ Chapter_10/educa/courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..models import Subject
+from ..models import Subject, Course, Module
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -25,3 +25,22 @@ class SubjectSerializer(serializers.ModelSerializer):
         
         model   = Subject
         fields  = ['id', 'title', 'slug']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model   = Module
+        fields  = ['order', 'title', 'description']
+        
+        
+class CourseSerializer(serializers.ModelSerializer):
+    
+    modules = ModuleSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model   = Course
+        fields  = ['id',
+                   'subject', 'title',
+                   'slug', 'overview', 'created',
+                   'owner', 'modules']
