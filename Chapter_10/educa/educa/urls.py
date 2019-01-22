244c02eceb10ec0ddfb9acdfd113a73632e88087
Ezio <assassinste@gmail.com>
2019-01-22 14:27:42 +0800

    Now you can access the 'subjects' by our APIs!

--- Chapter_10/educa/educa/urls.py
+++ Chapter_10/educa/educa/urls.py
@@ -18,6 +18,8 @@ urlpatterns = [
     path('course/',             include('courses.urls')),
     path('students/',           include('students.urls')),
     
+    path('api/',                include('courses.api.urls', namespace='api')),
+    
     path('',                    CourseListView.as_view(),           name='course_list'),
 ]
 
