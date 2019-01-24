f43fd36cca16fb1bf3fa00ad5d0e72ac76fa74d8
Ezio <assassinste@gmail.com>
2019-01-24 12:05:44 +0800

    Filtered added :: sensitive info :P

--- Chapter_10/educa/educa/settings/prod.py
+++ Chapter_10/educa/educa/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+from .whoa import *
 
 # Obviously, it's for prod-mode!
 DEBUG = False
@@ -6,13 +7,16 @@ DEBUG = False
 # All the errors that being raised
 #   will be sent to the admin user's email ( all of them :D )
 ADMINS = {
-    ('alex', '383138191@qq.com'),
+    (TEST_NAME, TEST_PSWD),
 }
 
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        # pass
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educa',
+        'USER': 'educa',
+        'PASSWORD': POSTGRESQL_PASSWORD,
     }
 }
