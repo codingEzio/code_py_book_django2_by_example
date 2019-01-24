abe15f8ddcdd0310bc0d9f12273fe6e7229de754
Ezio <assassinste@gmail.com>
2019-01-24 15:16:16 +0800

    Add config :: PostgreSQL

--- Chapter_10/educa/educa/settings/prod.py
+++ Chapter_10/educa/educa/settings/prod.py
@@ -14,9 +14,9 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'educa',
-        'USER': 'educa',
+        'ENGINE'  : 'django.db.backends.postgresql',
+        'NAME'    : 'educa',
+        'USER'    : 'educa',
         'PASSWORD': POSTGRESQL_PASSWORD,
     }
 }
